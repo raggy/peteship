dcbac7a0a37f77d2801c32e380bb62785106ece7
its.hip <its.hip@4dd0a116-9b8c-11dd-93bb-3f8bb607bf45>
2008-10-16 15:33:07 +0000

    Sorted out spacing

--- peteship.py
+++ peteship.py
@@ -18,77 +18,83 @@ darkgrey = 50, 50, 50
 pointsrgb = [red, green, blue]
 
 def D2R(degrees):
-      return degrees * (math.pi / 180)
-      #nigh on unholy goodness.
+    return degrees * (math.pi / 180)
+    #nigh on unholy goodness.
 
 def R2D(radians):
-      return (radians * 180) / math.pi
+    return (radians * 180) / math.pi
       
 class Ship():
-      #basic stats for drawing & position.
-      x = 50.0 #.0 for float plox.
-      y = 50.0
-      intRadius = 8
-      intRotation = 120
+    #basic stats for drawing & position.
+    x = 50.0 #.0 for float plox.
+    y = 50.0
+    intRadius = 8
+    intRotation = 120
    
-      #speed stats.
-      intSpeed = 0.01
-      intRotateSpeed =  0.1
+    #speed stats.
+    intSpeed = 0.01
+    intRotateSpeed =  0.1
 
-      #health.
-      intSI = 1
+    #health.
+    intSI = 1
 
-      #game side. e.g 4th player in 4 player match = side 3
-      intSide = 0
+    #game side. e.g 4th player in 4 player match = side 3
+    intSide = 0
 
-      #player owned?
-      boolPlayer=True
+    #player owned?
+    boolPlayer=True
 
-      points = []
+    points = []
+
+    hasOrder = False
+
+    def __init__(self, side, player=True):
+        self.side = side
+        self.player = player
+        self.calcPoints()
    
-      def draw(self):
-            #pygame.draw.line(screen, white, (50, 50), (25, 25))
-            for i in range(0, len(self.points)):
-                  if i == self.intEnginePoint:
-                        pygame.draw.aaline(screen, grey, self.points[i],self.points[i - 1])
-                  else:
-                        pygame.draw.aaline(screen, white, self.points[i],self.points[i - 1])
+    def draw(self):
+        #pygame.draw.line(screen, white, (50, 50), (25, 25))
+        for i in range(0, len(self.points)):
+            if i == self.intEnginePoint:
+                pygame.draw.aaline(screen, grey, self.points[i],self.points[i - 1])
+            else:
+                pygame.draw.aaline(screen, white, self.points[i],self.points[i - 1])
 
-      def rotateRight(self):
-            #Does exactly that.
-            self.intRotation += self.intRotateSpeed
-            if self.intRotation > 359:
-                  self.intRotation -= 360
+    def rotateRight(self):
+        #Does exactly that.
+        self.intRotation += self.intRotateSpeed
+        if self.intRotation > 359:
+            self.intRotation -= 360
 
-      def moveForward(self):
-            #LOLZ.
-            self.y -= math.degrees((math.cos(math.radians(self.intRotation))) * self.intSpeed)   
-            self.x += math.degrees((math.sin(math.radians(self.intRotation))) * self.intSpeed)
+    def moveForward(self):
+        #LOLZ.
+        self.y -= math.degrees((math.cos(math.radians(self.intRotation))) * self.intSpeed)   
+        self.x += math.degrees((math.sin(math.radians(self.intRotation))) * self.intSpeed)
 
+    def poll(self):
+            if not self.hasOrder:
+                  return False
+            else:
+                  return True
+
+            
 class S1s1(Ship):
-      intEnginePoint = 2
-      def __init__(self, side, player=True):
-            self.side = side
-            self.player = player
-            self.calcPoints()
+    intEnginePoint = 2
 
-      def calcPoints(self):
-            self.points = [(self.x + self.intRadius * math.sin(math.radians(self.intRotation)), self.y - self.intRadius * math.cos(math.radians(self.intRotation))),\
-                     (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 2.3 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 2.3 * math.pi / 3))),\
-                     (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 3.7 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 3.7 * math.pi / 3)))]
+    def calcPoints(self):
+        self.points = [(self.x + self.intRadius * math.sin(math.radians(self.intRotation)), self.y - self.intRadius * math.cos(math.radians(self.intRotation))),\
+        (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 2.3 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 2.3 * math.pi / 3))),\
+        (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 3.7 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 3.7 * math.pi / 3)))]
 
 class S1s2(Ship):
-      intEnginePoint = 2
-      def __init__(self, side, player=True):
-            self.side = side
-            self.player = player
-            self.calcPoints()
-
-      def calcPoints(self):
-            self.points = [(self.x + self.intRadius * math.sin(math.radians(self.intRotation)), self.y - self.intRadius * math.cos(math.radians(self.intRotation))),\
-            (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 1.7 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 1.7 * math.pi / 3))),\
-            (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 3 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 3 * math.pi / 3))),\
-            (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 4.3 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 4.3 * math.pi / 3)))]
+    intEnginePoint = 2
+      
+    def calcPoints(self):
+        self.points = [(self.x + self.intRadius * math.sin(math.radians(self.intRotation)), self.y - self.intRadius * math.cos(math.radians(self.intRotation))),\
+        (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 1.7 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 1.7 * math.pi / 3))),\
+        (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 3 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 3 * math.pi / 3))),\
+        (self.x + self.intRadius * math.sin((math.radians(self.intRotation) + 4.3 * math.pi / 3)), self.y - self.intRadius * math.cos((math.radians(self.intRotation) + 4.3 * math.pi / 3)))]
 
 
 ships = [S1s1(0, True), S1s2(0, True)]
@@ -100,17 +106,16 @@ running = 1
 GC.start()
 
 while running:
-      for frame_count, game_time in GC.update():
-            event = pygame.event.poll()
-            if event.type == pygame.QUIT:
-                  running = 0
-                  pygame.quit()
+    for frame_count, game_time in GC.update():
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            running = 0
+            pygame.quit()
+    
+        screen.fill(black)
             
-            screen.fill(black)
-            
-            for ship in ships:
-                  ship.moveForward()
-                  ship.calcPoints()
-                  ship.draw()
+        for ship in ships:
+            ship.poll()
+            ship.draw()
                   
-            pygame.display.flip()
+        pygame.display.flip()
