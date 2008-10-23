1aab5a7fdf076cedf4d32a30a9e7aba44d2a0db8
Mr.PeteLord <Mr.PeteLord@4dd0a116-9b8c-11dd-93bb-3f8bb607bf45>
2008-10-23 09:28:34 +0000

    Added some build code, currently broken but not crashing game.
    Changed intRotation of Ship to rotation.
    
    Fixxed onscreen render code. Needs better Idling :P

--- peteship.py
+++ peteship.py
@@ -4,8 +4,10 @@ pygame.init()
 
 """ rev12 : set framerate to 30, hopefully """
 clock = pygame.time.Clock()
-size = width, height = 800, 480
+size = width, height = 800, 480 #Eee compatible resolution. ;)
 screen = pygame.display.set_mode(size)
+
+GLOBAL_TESTSHIPS = 1 #Generic int for creating multimples of tsetingships.
 
 black = 0, 0, 0
 white = 255, 255, 255
@@ -57,23 +59,24 @@ class MoveToXY(Order):
         #pygame.draw.circle(screen, midgreen, ((int(self.x * player.zoom) - player.x), (int(self.y) * player.zoom) - player.y), ship.radius - 3, 2) # circle designators for the move. Currently living above ships so needs to be changed.
         pygame.draw.line(screen, (20,20,20), ((int(self.x * player.zoom) - player.x), (int(self.y * player.zoom)) - player.y), ((ship.x * player.zoom) - player.x,(ship.y * player.zoom) - player.y))
         # New behaviour, rotate whilst moving
-        if ship.distanceFrom(self.x, self.y) > math.sqrt(((ship.x + math.sin(ship.intRotation) * ship.intSpeed)-self.x)**2 + ((ship.y - math.cos(ship.intRotation) * ship.intSpeed)-self.y)**2): # If next move will bring you closer to the destination
-            if normalisedAngle(self.angleToXY - ship.intRotation) < (math.pi / 4) or normalisedAngle(self.angleToXY - ship.intRotation) > (math.pi * 1.75):
+        if ship.distanceFrom(self.x, self.y) > math.sqrt(((ship.x + math.sin(ship.rotation) * ship.intSpeed)-self.x)**2 + ((ship.y - math.cos(ship.rotation) * ship.intSpeed)-self.y)**2): # If next move will bring you closer to the destination
+            if normalisedAngle(self.angleToXY - ship.rotation) < (math.pi / 4) or normalisedAngle(self.angleToXY - ship.rotation) > (math.pi * 1.75):
                 if (ship.x, ship.y) != (self.x, self.y): # stop the ship on target
                     if ship.distanceFrom(self.x, self.y) < ship.intSpeed: # If the destintion is a shorter distance than the move distance...
                         ship.order = Idle(ship) # dump orders and...
                     ship.moveForward()          # cover the rest of the distance.
                 else:
                     ship.order = Idle(ship)     # if all else fails, dump orders.
-        if ship.intRotation != self.angleToXY: # If the ship isn't already facing the right way
+        if ship.rotation != self.angleToXY: # If the ship isn't already facing the right way
             self.angleToXY = ship.angleToXY(self.x, self.y)
             ship.rotateTowardAngle(self.angleToXY) # then rotate towards the right way
         # always recalculate points after moving. Rev 23: This is the source of the speedups slowdownsin ships. Calcpoints is part of the render loop.
       
 class Ship():
     #basic stats for drawing & position.
-    radius = 8                     # Size of the ship from the centre - size of largest part (if multiple parts are added)
-    intRotation = math.radians(270.0) # Initial rotation of the ship. Changes every now and then for testing, doesn't matter usually.
+    radius = 8                      # Size of the ship from the centre - size of largest part (if multiple parts are added)
+    rotation = math.radians(270.0)  # Initial rotation of the ship. Changes every now and then for testing, doesn't matter usually.
+                                    # r43 : Changed to rotation instead of intRotation
     
     #speed stats.
     intSpeed = 2.5       # Movement
@@ -97,20 +100,20 @@ class Ship():
         pygame.draw.aalines(screen, player.colour, True, self.offsetPoints())
         
     def rotateTowardAngle(self, angle):
-        if positive(angle - ship.intRotation) < ship.intRotateSpeed: # If rotation speed is bigger than the amount which you need to turn
-            self.intRotation = angle # then only turn to face the desired angle
+        if positive(angle - ship.rotation) < ship.intRotateSpeed: # If rotation speed is bigger than the amount which you need to turn
+            self.rotation = angle # then only turn to face the desired angle
         else:
-            if normalisedAngle(angle - self.intRotation) > math.pi: # If the angle which you're rotating towards is more 180 degrees to the right, it makes more sense to turn left
-                self.intRotation = normalisedAngle(self.intRotation - self.intRotateSpeed) # Turn left by self.intRotateSpeed
+            if normalisedAngle(angle - self.rotation) > math.pi: # If the angle which you're rotating towards is more 180 degrees to the right, it makes more sense to turn left
+                self.rotation = normalisedAngle(self.rotation - self.intRotateSpeed) # Turn left by self.intRotateSpeed
             else:
-                self.intRotation = normalisedAngle(self.intRotation + self.intRotateSpeed) # Turn right by self.intRotateSpeed
+                self.rotation = normalisedAngle(self.rotation + self.intRotateSpeed) # Turn right by self.intRotateSpeed
 
     def moveForward(self, speed=0):
         #LOLZ.
         if speed == 0:
             speed = self.intSpeed             
-        self.y -= math.cos(self.intRotation) * speed  
-        self.x += math.sin(self.intRotation) * speed
+        self.y -= math.cos(self.rotation) * speed  
+        self.x += math.sin(self.rotation) * speed
 
     def poll(self):
         #update the ships data
@@ -140,38 +143,72 @@ class S1s1(Ship):
     """ as of rev 12 now a list"""
     intEnginePoint = [2]
 
+    #buildInfo
+    buildCost = 10
+    buildTime = 1000
+
     def calcPoints(self):
     #calculate the three points of the triangle relative to the center xy of the ship
     #and the radius given to the ship.
-        self.points = [(self.x + self.radius * math.sin(self.intRotation), (self.y - self.radius * math.cos(self.intRotation))),\
-        (self.x + self.radius * math.sin(self.intRotation + 2.3 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 2.3 * math.pi / 3))),\
-        (self.x + self.radius * math.sin(self.intRotation + 3.7 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 3.7 * math.pi / 3)))]
+        self.points = [(self.x + self.radius * math.sin(self.rotation), (self.y - self.radius * math.cos(self.rotation))),\
+        (self.x + self.radius * math.sin(self.rotation + 2.3 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 2.3 * math.pi / 3))),\
+        (self.x + self.radius * math.sin(self.rotation + 3.7 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 3.7 * math.pi / 3)))]
 
 class S1s2(Ship):
     """ as of rev 12, now a list """
     intEnginePoint = [2, 3]
+
+    #buildInfo
+    buildCost = 10
+    buildTime = 1000
       
     def calcPoints(self):
-        self.points = [((self.x + self.radius * math.sin(self.intRotation)), (self.y - self.radius * math.cos(self.intRotation))),\
-        (self.x + self.radius * math.sin(self.intRotation + 1.7 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 1.7 * math.pi / 3))),\
-        (self.x + self.radius * math.sin(self.intRotation + 3 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 3 * math.pi / 3))),\
-        (self.x + self.radius * math.sin(self.intRotation + 4.3 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 4.3 * math.pi / 3)))]
+        self.points = [((self.x + self.radius * math.sin(self.rotation)), (self.y - self.radius * math.cos(self.rotation))),\
+        (self.x + self.radius * math.sin(self.rotation + 1.7 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 1.7 * math.pi / 3))),\
+        (self.x + self.radius * math.sin(self.rotation + 3 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 3 * math.pi / 3))),\
+        (self.x + self.radius * math.sin(self.rotation + 4.3 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 4.3 * math.pi / 3)))]
 
 class S1s6(Ship):
     """ Carrier """
     intEnginePoint = [0, 0]
     buildPoints = []
+    buildQueue = []
+    building = False
+    buildTimeRemaining = 0
+    buildShip = Ship
+
+    #buildInfo
+    buildCost = 10
+    buildTime = 1000
         
     def calcPoints(self):
-        self.points = [(self.x + self.radius * math.sin(self.intRotation + 5.8 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 5.8 * math.pi /3))),\
-        (self.x + self.radius * math.sin(self.intRotation + 0.2 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 0.2 * math.pi / 3))),\
-        (self.x + self.radius * math.sin(self.intRotation + 2 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 2 * math.pi / 3))),\
-        (self.x + self.radius * math.sin(self.intRotation + 2.8 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 2.8 * math.pi / 3))),\
-        (self.x + self.radius * math.sin(self.intRotation + 3.2 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 3.2 * math.pi / 3))),\
-        (self.x + self.radius * math.sin(self.intRotation + 4 * math.pi / 3), (self.y - self.radius * math.cos(self.intRotation + 4 * math.pi / 3)))]
+        self.points = [(self.x + self.radius * math.sin(self.rotation + 5.8 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 5.8 * math.pi /3))),\
+        (self.x + self.radius * math.sin(self.rotation + 0.2 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 0.2 * math.pi / 3))),\
+        (self.x + self.radius * math.sin(self.rotation + 2 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 2 * math.pi / 3))),\
+        (self.x + self.radius * math.sin(self.rotation + 2.8 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 2.8 * math.pi / 3))),\
+        (self.x + self.radius * math.sin(self.rotation + 3.2 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 3.2 * math.pi / 3))),\
+        (self.x + self.radius * math.sin(self.rotation + 4 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 4 * math.pi / 3)))]
         
-        self.buildPoints.append([(self.x + (self.radius + 10) * math.sin(self.intRotation)), (self.y - (self.radius + 10) * math.cos(self.intRotation))])
+        self.buildPoints.append([(self.x + (self.radius + 10) * math.sin(self.rotation)), (self.y - (self.radius + 10) * math.cos(self.rotation))])
 
+    def poll(self):
+        #standard poll functions
+        self.order.poll()
+        if self.building == False and len(self.buildQueue) > 0:
+            self.buildShip = self.buildQueue.pop(0)
+            ships.append(self.buildShip) # Add to list of ships.
+            self.player.resources -= self.buildShip.buildCost
+            self.buildTimeRemaining = self.buildShip.buildTime
+            self.building = True
+        elif self.building == True:
+            print "bldng"
+            self.buildTimeRemaining -= 1
+            self.buildShip.x = self.buildPoints[0][0]
+            self.buildShip.y = self.buildPoints[0][1]
+
+    def addToBuildQueue(self): #Currently only produces triangles.
+        buildQueue.append(S1s1(self.player, buildPoints[0][0], buildPoints[0][1])) # Broken, wont add to list?
+                          
 """ New in r27 """
 class Player(): 
     """ Set of stats to store what the player can see. """
@@ -189,6 +226,7 @@ class Player():
     """ Player specific stats. """
     colour = white # hahaha why not.
     name = "Ronco"
+    resources = 9001 # MONEY, GET BACK.
     """ End of player specific stats """
     def __init__(self):
         self.calcBounds()
@@ -206,22 +244,29 @@ player = Player()
 
 """
 ships = [S1s1(player, 100.0, 50.0), S1s2(player, 100.0, 100.0), S1s1(player, 150, 75)]
-ships[0].intRotation = math.radians(270)
-ships[1].intRotation = math.radians(269)
+ships[0].rotation = math.radians(270)
+ships[1].rotation = math.radians(269)
 ships[0].order = MoveToXY(ships[0], 300.0, 50.0)
 ships[1].order = MoveToXY(ships[1], 500.0, 100.0)
 ships[2].order = MoveToXY(ships[2], 152.0, 75.0)
 """
 
 ships = []
-for i in range(1000):
-    ships.append(S1s1(player, (random.random()*width), (random.random()*height)))
+for i in range(GLOBAL_TESTSHIPS): # GLOBAL_TESTSHIPS is located at the top, this is a pain to find sometimes.
+    ships.append(S1s6(player, (random.random()*width), (random.random()*height)))
     ships[i].order = MoveToXY(ships[i], 100.0, 100.0)
+
+""" build test code """
+#!Warning! ships[0] must be of class S1s6 or greater. !Warning!
+ships[0].addToBuildQueue
+print ships[0].buildQueue
+
+""" end build test code """
 
 running = True
 
 while running:
-    clock.tick(60)
+    clock.tick(30)
     pygame.msg.message
     pygame.event.clear(pygame.MOUSEMOTION)
     pygame.event.clear(pygame.MOUSEBUTTONUP)
@@ -247,21 +292,21 @@ while running:
         elif event.key == pygame.K_ESCAPE:
             pygame.quit()
             running = False
-        elif event.key == pygame.K_q:
+        elif event.key == pygame.K_q: # petenote: When i figure out how many pixels this changes by i'll move the display so the zoom is centered.
             player.zoom -= 0.1
         elif event.key == pygame.K_w:
             player.zoom += 0.1
-    screen.fill(black)
 
-    for ship in ships: # Need to do code to check whether ships are on screen before drawing them
+    screen.fill(black) #ARRR.
+    
+    #Update ships x & y. If the ship is onscreen, draw it. If it's not moving, don't calculate the points again = saves proc time. Will lag more and more with more ships moving though.
+    for ship in ships: # Rev 43: Will work better when ships Idle properly. At the moment they stay with a move order.
         ship.poll()
-        if ship.x > player.lBound:
-            if ship.x < player.rBound:
-                if ship.y > player.tBound:
-                    if ship.y < player.bBound:
-                        if ship.order != Idle:
-                            ship.calcPoints()
-                        ship.draw()
+        if ship.x > player.lBound and ship.x < player.rBound and ship.y > player.tBound and ship.y < player.bBound:
+            if not isinstance(ship.order,Idle):
+                #print ship.order #debug to see if ships stop.
+                ship.calcPoints()
+            ship.draw()
     
     pygame.display.flip()
     for event in pygame.event.get(pygame.QUIT):
