970691c29c3ea575a04b56414770e9c497cbd64c
Mr.PeteLord <Mr.PeteLord@4dd0a116-9b8c-11dd-93bb-3f8bb607bf45>
2008-10-24 13:44:30 +0000

    Working with formations

--- formations.py
+++ formations.py
@@ -4,11 +4,31 @@ class Formation:
     pattern = 0
     ships = []
     xys = []
-    def __init__(self, shipList, pattern=0):
+    rotation = 0
+    width = 20
+    orders = [peteship.Idle()]
+    def __init__(self, shipList, pattern=0, width = 20):
         self.ships = shipList
         self.pattern = pattern
-        xys[0] = (self.ships[0].x, self.ships[0].y)
-        
-
+        self.xys[0] = (self.ships[0].x, self.ships[0].y)
+        self.rotation = self.ships[0].rotation
+        self.orders = self.ships[0].orders
+    
     def calcPattern(self):
-        pass
+        x = 0
+        if pattern = 0 # line formation, left to right.
+            for order in orders:
+                
+                if isinstance(order, peteship.MoveToXY):
+            """        
+            for i = range(1 to (len(self.ships) - 1):
+                self.xys[i] = (self.x + (self.width * i) * math.sin(self.rotation + 3 * math.pi / 3), (self.y - self.radius * math.cos(self.rotation + 3 * math.pi / 3)
+            for i = range(0 to (len(self.ships) - 1):
+                if isinstance(self.orders[0], peteship.MoveToXY)
+                      self.ships[i].setOrder(
+          """
+
+            
+
+    def poll(self): # if the orders change, this can be called to recalc nicely.
+        calcPattern()
