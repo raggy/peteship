ed7f145155ccf29aa102094ff2f3668829ebfd1d
Mr.PeteLord <Mr.PeteLord@4dd0a116-9b8c-11dd-93bb-3f8bb607bf45>
2008-11-04 20:22:04 +0000

    Basic missiles in, needs muchos tweaking.

--- trunk/weapons.py
+++ trunk/weapons.py
@@ -1,20 +1,25 @@
-import players, pygame, ships, orders
+import players, pygame, ships, orders, math
 
-class Missile(Ship):
-    def __init__(self, hardPoint, targetShip):
+class Missile(ships.Ship):
+    def __init__(self, player, hardPoint, targetShip):
+        self.built = True # always built.
+        self.moving = True # always moving, unless it's hit.
+        self.player = player
         self.x = hardPoint[0]
         self.y = hardPoint[1]
         self.rotation = hardPoint[2]
         self.setOrder(orders.MoveToShip(targetShip))
         
-        
     def draw(self):
         #calculate a simple single line to show the missile. Default thing.
-        self.points = [(self.x + self.radius * math.sin(self.rotation), (self.y - self.radius * math.cos(self.rotation)),(self.x, self.y)]
+        if self.needsToCalcPoints:
+            self.calcPoints()
+        pygame.draw.aalines(self.player.screen, self.player.colour, False, self.offsetPoints())
+        
+    def calcPoints(self):
+        self.points = [(self.x + self.radius * math.sin(self.rotation), (self.y - self.radius * math.cos(self.rotation))),(self.x, self.y)]
         self.needsToCalcPoints = False
         
     def poll(self):
         self.orders[0].poll()
         self.calcExtras
-        
-    
