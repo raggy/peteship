ecb527b0abc4a67f5a1be5d939c980d10019bd98
its.hip <its.hip@4dd0a116-9b8c-11dd-93bb-3f8bb607bf45>
2008-11-06 21:16:25 +0000

    Added several hacks for stars

--- peteship.py
+++ peteship.py
@@ -115,11 +115,21 @@ def main(player, MAPWIDTH, MAPHEIGHT): # NEEDS MAP HEIGHT! MAKES GAME BIGGER, DE
     # Draw stars. Sticking it all in one place to reduce calls.
         if player.drawStars:
             for star in player.upperStars:
+		colour = (150 * player.zoom, 150 * player.zoom, 150 * player.zoom)
+		if colour[0] > 255:
+		    colour = (255, 255, 255)
                 if star[0] > player.lBound and star[0] < player.rBound and star[1] > player.tBound and star[1] < player.bBound:
-                    pygame.draw.line(player.screen, star[2], ((star[0] - player.x)*player.zoom, (star[1] - player.y)*player.zoom), ((star[0] - player.x)*player.zoom, (star[1] - player.y)*player.zoom))
-            for star in player.lowerStars:
-                if star[0] > player.lBound and star[0] < player.rBound and star[1] > player.tBound and star[1] < player.bBound:
-                    pygame.draw.line(player.screen, star[2], ((star[0] - player.x)*player.zoom * 0.7, (star[1] - player.y)*player.zoom * 0.7), ((star[0] - player.x)*player.zoom * 0.7, (star[1] - player.y)*player.zoom * 0.7))
+                    pygame.draw.line(player.screen, colour, ((star[0] - player.x)*player.zoom, (star[1] - player.y)*player.zoom), ((star[0] - player.x)*player.zoom, (star[1] - player.y)*player.zoom))
+	    if player.width / player.zoom <= misc.GLOBAL_MAPWIDTH and player.height / player.zoom <= misc.GLOBAL_MAPHEIGHT:
+		colour = (90 * player.zoom, 90 * player.zoom, 90 * player.zoom)
+		if colour[0] > 255:
+		    colour = (255, 255, 255)
+		for star in player.lowerStars:
+		    if star[0] * 0.7 > player.x * 0.7 and\
+		    star[0] * 0.7 < player.x * 0.7 + player.width / player.zoom and\
+		    star[1] * 0.7 > player.y * 0.7 and\
+		    star[1] * 0.7 < player.y * 0.7 + player.height / player.zoom:
+			pygame.draw.line(player.screen, colour, ((star[0] * 0.7 - player.x * 0.7) * player.zoom, (star[1] * 0.7 - player.y * 0.7) * player.zoom), ((star[0] * 0.7 - player.x * 0.7) * player.zoom, (star[1] * 0.7 - player.y * 0.7) * player.zoom))
 
 	# Draw contrails.
         for effect in player.lowEffects:
